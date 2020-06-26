701ac4eb456ed27e24424d983de4e79f7ad71dfa
karinafitriwulan <61909973+karinafitriwulan@users.noreply.github.com>
2020-06-27 02:19:44 +0800

    Add files via upload

---
+++ src/com/kruwell/program/datamhs.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kruwell.program;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author HP14s
+ */
+public class datamhs {
+    
+    private static Connection MySQLConfig;
+    
+    public static Connection configDB()throws SQLException{
+        try{
+            String url = "jdbc:mysql://localhost:3306/Mahasiswa";
+            String user = "root";
+            String pass = "";
+            
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            MySQLConfig = DriverManager.getConnection(url, user, pass);
+        }catch(SQLException e){
+            System.out.println("Maaf, koneksi ke database gagal" + e.getMessage());
+        }
+        return MySQLConfig;
+    }
+}
